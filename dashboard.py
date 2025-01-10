d9aada5a745740f59eb88cb4fa6092cb79e29f9d
ThomasBredee <thomasbb@hotmail.nl>
2025-01-10 11:08:03 +0100

    Dashboard push

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+
+st.set_page_config(page_title = 'Collaboration Dashboard', page_icon = ":bar_chart")
