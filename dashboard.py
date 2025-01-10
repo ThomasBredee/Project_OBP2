246bca52d0998512c8261fbe67c4fcf66d8cc3cf
ThomasBredee <thomasbb@hotmail.nl>
2025-01-10 12:59:08 +0100

    Dashboard push 2

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,39 @@
 import streamlit as st
 import pandas as pd
-import os
 import warnings
 warnings.filterwarnings('ignore')
 
+#RUNCOMMAND
+#streamlit run C:\Users\thoma\PycharmProjects\Project_OBP\dashboard.py
 
-st.set_page_config(page_title = 'Collaboration Dashboard', page_icon = ":bar_chart")
+#installs:
+#pip install pandas
+#pip install streamlit
+#pip install openpyxl
+
+st.set_page_config(page_title = 'Collaboration Dashboard', page_icon = ":bar_chart", layout = 'wide')
+st.title(" :bar_chart: Collaboration Dashboard")
+
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>',unsafe_allow_html=True)
+
+# File uploader
+fl = st.file_uploader(":file_folder: Upload a file", type=(["csv", "txt", "xlsx", "xls"]))
+
+if fl is not None:
+    # Use the file-like object directly
+    if fl.name.endswith('.csv'):
+        df = pd.read_csv(fl, encoding="ISO-8859-1")
+    elif fl.name.endswith(('.xlsx', '.xls')):
+        df = pd.read_excel(fl, engine='openpyxl')
+    else:
+        st.error("Unsupported file type!")
+        st.stop()
+
+    st.write(f"Uploaded file: {fl.name}")
+    st.dataframe(df)  # Display the dataframe
+
+
+
+
+
+
